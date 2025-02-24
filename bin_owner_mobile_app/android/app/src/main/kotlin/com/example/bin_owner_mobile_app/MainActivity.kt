f0da8781e519c4995a63526bc04b801568e766fe
Kamesh Chanuka <chanukabdk@gmail.com>
2025-02-24 10:28:50 +0530

    Created mobile apps for bin owner and truck driver

---
+++ bin_owner_mobile_app/android/app/src/main/kotlin/com/example/bin_owner_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bin_owner_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ truck_driver_mobile_app/android/app/src/main/kotlin/com/example/truck_driver_mobile_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.truck_driver_mobile_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
