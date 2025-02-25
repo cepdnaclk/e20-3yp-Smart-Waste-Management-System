4ee0518313bd57f6f0a6fa3861a03cb16984aa32
Kamesh Chanuka <chanukabdk@gmail.com>
2025-02-25 18:34:23 +0530

    Added backend project

---
+++ greenpulse-backend/src/test/java/com/greenpulse/greenpulse_backend/GreenpulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenpulse.greenpulse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenpulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
