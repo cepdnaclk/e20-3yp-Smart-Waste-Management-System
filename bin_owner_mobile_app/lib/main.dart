5e8ab9f79b8674c7a98a65fa5f6bf803b87cb1f2
Kamesh Chanuka <chanukabdk@gmail.com>
2025-02-25 15:32:50 +0530

    Added routes.dart file to store routes of the app. Added bin_level_screen.dart page to show bin levels.
    - bin_level_screen.dart is added but no widgets are included in the
      page. Features are yet to be added.

--- bin_owner_mobile_app/lib/main.dart
+++ bin_owner_mobile_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bin_owner_mobile_app/routes.dart';
 import 'package:flutter/material.dart';
 
 import './theme/theme.dart';
@@ -12,9 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'GreenPulse',
       theme: customTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: "Home Page"),
+      initialRoute: AppRoutes.home,
+      routes: AppRoutes.routes,
     );
   }
 }
@@ -28,14 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,18 +39,14 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.binLevel);
+              },
+              child: Text("Bin Status Page"),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ bin_owner_mobile_app/lib/screens/bin_level_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BinLevelScreen extends StatelessWidget {
+  const BinLevelScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Bin Level Monitor')));
+  }
+}
