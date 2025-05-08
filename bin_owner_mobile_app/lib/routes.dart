062fbc494867220ed8654a351315995ece92b301
hasini416 <e20271@eng.pdn.ac.lk>
2025-05-08 17:02:01 +0530

    login screen created and add to the route

--- bin_owner_mobile_app/lib/routes.dart
+++ bin_owner_mobile_app/lib/routes.dart
@@ -1,12 +1,18 @@
 import 'package:bin_owner_mobile_app/main.dart';
 import 'package:flutter/material.dart';
 import './screens/bin_level_screen.dart';
+import './screens/login_screen.dart';
 
 class AppRoutes {
+  // Authentication
+  static const String login = '/login';
+
   static const String home = '/home';
   static const String binLevel = '/bin-level-screen';
 
   static Map<String, WidgetBuilder> routes = {
+    login: (context) => const LoginScreen(),
+
     home: (context) => MyHomePage(title: 'Home Page'),
     binLevel: (context) => BinLevelScreen(),
   };
