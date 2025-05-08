68a96bd9dc166291e7e154967f3c5f05d798aa7e
hasini416 <e20271@eng.pdn.ac.lk>
2025-05-08 17:32:46 +0530

    notification page is created and route and main is updated

--- bin_owner_mobile_app/lib/routes.dart
+++ bin_owner_mobile_app/lib/routes.dart
@@ -1,13 +1,16 @@
 import 'package:bin_owner_mobile_app/main.dart';
 import 'package:flutter/material.dart';
 import './screens/bin_level_screen.dart';
+import './screens/notification.dart';
 
 class AppRoutes {
   static const String home = '/home';
   static const String binLevel = '/bin-level-screen';
+  static const String notifications = '/notifications';
 
   static Map<String, WidgetBuilder> routes = {
     home: (context) => MyHomePage(title: 'Home Page'),
     binLevel: (context) => BinLevelScreen(),
+    notifications: (context) => const NotificationScreen(),
   };
 }
