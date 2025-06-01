5d536f36af6795135298f16d61df35943d8038c5
Pasindu Malshan <e20244@eng.pdn.ac.lk>
2025-06-01 17:15:49 +0530

    login integrated to backend

---
+++ greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.greenpulse.greenpulse_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173/") // Change to specific origin(s) in production
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
