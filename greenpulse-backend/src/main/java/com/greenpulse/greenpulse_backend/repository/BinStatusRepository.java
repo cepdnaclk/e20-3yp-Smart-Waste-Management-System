261f5c957af93f7ed3d28c931fb45d7c75abf235
Kamesh Chanuka <e20049@eng.pdn.ac.lk>
2025-06-02 11:46:09 +0530

    Fetch bins by bin owners Added an endpoint to fetch the bin status of a registered bin owner

--- greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/repository/BinStatusRepository.java
+++ greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/repository/BinStatusRepository.java
@@ -1,7 +1,9 @@
 package com.greenpulse.greenpulse_backend.repository;
 
+import com.greenpulse.greenpulse_backend.model.BinInventory;
 import com.greenpulse.greenpulse_backend.model.BinStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BinStatusRepository extends JpaRepository<BinStatus, String> {
+    String bin(BinInventory bin);
 }
--- greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/service/BinStatusService.java
+++ greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/service/BinStatusService.java
@@ -1,24 +1,68 @@
 package com.greenpulse.greenpulse_backend.service;
 
+import com.greenpulse.greenpulse_backend.dto.ApiResponse;
 import com.greenpulse.greenpulse_backend.dto.BinStatusDTO;
 import com.greenpulse.greenpulse_backend.exception.BinStatusNotFoundException;
+import com.greenpulse.greenpulse_backend.exception.UserNotFoundException;
+import com.greenpulse.greenpulse_backend.model.BinInventory;
 import com.greenpulse.greenpulse_backend.model.BinStatus;
+import com.greenpulse.greenpulse_backend.model.UserTable;
+import com.greenpulse.greenpulse_backend.repository.BinInventoryRepository;
 import com.greenpulse.greenpulse_backend.repository.BinStatusRepository;
+import com.greenpulse.greenpulse_backend.repository.UserTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class BinStatusService {
 
     private final BinStatusRepository binStatusRepository;
+    private final BinInventoryRepository binInventoryRepository;
+    private final UserTableRepository userTableRepository;
 
     @Autowired
     public BinStatusService(
-            BinStatusRepository binStatusRepository
+            BinStatusRepository binStatusRepository,
+            BinInventoryRepository binInventoryRepository,
+            UserTableRepository userTableRepository
     ) {
         this.binStatusRepository = binStatusRepository;
+        this.binInventoryRepository = binInventoryRepository;
+        this.userTableRepository = userTableRepository;
+    }
+
+    public ApiResponse<BinStatusDTO> getBinStatus(String binId, UUID userId) {
+        BinInventory binInventory = binInventoryRepository.findById(binId)
+                .orElseThrow(() -> new BinStatusNotFoundException(binId));;
+
+        UserTable user =  userTableRepository.findById(binInventory.getOwner().getUserId())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        if(!user.getId().equals(userId)) {
+            throw new UserNotFoundException("User not found for given bin");
+        }
+
+        BinStatus binStatus = binStatusRepository.findById(binId)
+                .orElseThrow(() -> new BinStatusNotFoundException(binId));
+
+        BinStatusDTO binStatusDTO = new BinStatusDTO();
+
+        binStatusDTO.setBinId(binId);
+        binStatusDTO.setPlasticLevel(binStatus.getPlasticLevel());
+        binStatusDTO.setGlassLevel(binStatus.getGlassLevel());
+        binStatusDTO.setPaperLevel(binStatus.getPaperLevel());
+
+        return ApiResponse.<BinStatusDTO>builder()
+                .success(true)
+                .message("Bin status fetched successfully")
+                .data(binStatusDTO)
+                .timestamp(LocalDateTime.now().toString())
+                .build();
+
     }
 
     public void updateBinLevels(BinStatusDTO binStatusDTO) {
