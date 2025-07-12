059291df2a2eec4ce70b488190e1b660d36c9f6f
Madhura Bashitha <e20093@eng.pdn.ac.lk>
2025-07-12 09:31:53 +0530

    Add backend route and stop functionalities for truck driver app

--- greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/repository/BinStatusRepository.java
+++ greenpulse-backend/src/main/java/com/greenpulse/greenpulse_backend/repository/BinStatusRepository.java
@@ -1,11 +1,16 @@
 package com.greenpulse.greenpulse_backend.repository;
 
+import aj.org.objectweb.asm.commons.Remapper;
 import com.greenpulse.greenpulse_backend.model.BinInventory;
 import com.greenpulse.greenpulse_backend.model.BinStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BinStatusRepository extends JpaRepository<BinStatus, String> {
     String bin(BinInventory bin);
+
+    Optional<BinStatus> findByBinId(String binId);
 }
